04ae0fe395c54380979195eff677c07fe46eda0a
zhuweitung <zhuweitung@foxmail.com>
2026-07-10 13:35:25 +0800

    feat: 更换应用启动图标

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.roco.catcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildFeatures {
